9e68eef2ff8ebc51d12b98362687832355161ee9
Jake Anthony Sganga <sganga@stanford.edu>
2017-02-27 23:27:58 +0000

    added more comments

--- arduino_plotter.py
+++ arduino_plotter.py
@@ -51,6 +51,7 @@ class AnalogPlot(object):
     def update(self, frameNum):
       try:
           stream = self.arduino.readline()
+          ### if you want to plot in real-time (no buffer), uncomment next two lines
           # while self.arduino.inWaiting() > 0: # clears buffer
           #     stream = self.arduino.readline()
           stream_values = [float(val) for val in stream.split()]
@@ -98,11 +99,18 @@ def main(port, baud_rate):
 
     print('exiting.')
   
+"""
+call main, CHANGE THESE VALUES!!!
+replace 'COM6' with whatever the tools pulldown bar shows you're connected to
+for mac, it looks like '/dev/cu.usbmodem....' (without the (Arduino/Genuino Micro))
+Also, note the arduino puts out data faster than we can plot in real time, so there's 
+a delay between the plotted value and the actual value (~1-2 min)
 
-# call main, CHANGE THESE VALUES!!!
-# replace 'COM6' with whatever the tools pulldown bar shows you're connected to
-# for mac, it looks like '/dev/cu.usbmodem....' (without the (Arduino/Genuino Micro))
-# Also, note the arduino puts out data faster than we can plot in real time, so there's 
-# a delay between the plotted value and the actual value (~1-2 min)
+Format for data in Arduino Serial print, data values separated by space, new line for each time point:
+Serial.print(data_value_1);
+Serial.print(' ');
+...
+Serial.println(data_value_n);
+"""
 if __name__ == '__main__':
     main('COM6', 9600)
